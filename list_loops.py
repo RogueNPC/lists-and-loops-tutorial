4ff27c969dcb37189a4a3e59633f1c02f24e9e79
RogueNPC <jordanbtansay@gmail.com>
2020-09-22 12:11:20 -0700

    Q4 Answered

--- list_loops.py
+++ list_loops.py
@@ -1,7 +1,17 @@
+#Q1
 songs = ["ROCKSTAR", "Do It", "For The Night"]
+
+#Q2
 print(songs[0])
 print(songs[2])
 print(songs[1:])
 
+#Q3
 songs[2] = "Army of Two"
 print(songs)
+
+#Q4
+songs.extend (["Message in the Wind", "Threads", "Not Afraid"])
+del songs[1]
+print(songs)
+
